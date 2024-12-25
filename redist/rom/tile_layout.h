94b82db4f35a402d2c09025ee77ac00cc8c80205
Jason Harmon <jharmon891@yahoo.co.uk>
2024-12-25 22:02:31 +0000

    Add TileLayout deserialisation and stub additional related types

--- redist/rom/tile_layout.h
+++ redist/rom/tile_layout.h
@@ -3,6 +3,7 @@
 #include "rom_data.h"
 #include <vector>
 #include <memory>
+#include <array>
 
 namespace spintool::rom
 {
@@ -22,12 +23,33 @@ namespace spintool::rom
 
 	};
 
+	struct TileBrush
+	{
+		constexpr static const size_t s_brush_width = 16;
+		constexpr static const size_t s_brush_height = 16;
+		constexpr static const size_t s_brush_total_tiles = s_brush_width * s_brush_height;
+
+		ROMData rom_data;
+		std::array <TileInstance, TileBrush::s_brush_total_tiles> tiles;
+	};
+
+	struct TileBrushInstance
+	{
+		Uint16 x_offset = 0;
+		Uint16 y_offset = 0;
+		TileBrush tile_brush;
+	};
+
 	struct TileLayout
 	{
 		ROMData rom_data;
 
 		int layout_width = 0;
 		int layout_height = 0;
+
+		std::vector<TileBrush> tile_brushes;
+		std::vector<TileBrushInstance> tile_brush_instances;
+
 		std::vector<TileInstance> tile_instances;
 
 		static std::shared_ptr<TileLayout> LoadFromROM(const SpinballROM& src_rom, size_t offset);
--- src/rom/tile_layout.cpp
+++ src/rom/tile_layout.cpp
@@ -1,9 +1,40 @@
 #include "rom/tile_layout.h"
 
+#include "rom/spinball_rom.h"
+
 namespace spintool::rom
 {
 	std::shared_ptr<spintool::rom::TileLayout> TileLayout::LoadFromROM(const SpinballROM& src_rom, size_t offset)
 	{
-		return nullptr;
+		const Uint8* start_byte = &src_rom.m_buffer[offset];
+		const Uint8* current_byte = start_byte;
+
+		auto new_layout = std::make_shared<TileLayout>();
+		new_layout->layout_width = 40;//48;
+		new_layout->layout_height = 28;
+
+		const auto total_tiles = new_layout->layout_width * new_layout->layout_height;
+		new_layout->tile_instances.reserve(total_tiles);
+
+		for (size_t i = 0; i < 0x23fd /*0x146*/; ++i)
+		{
+			const Uint8 first_byte = *current_byte;
+			++current_byte;
+			const Uint8 second_byte = *current_byte;
+			++current_byte;
+
+			const Uint16 combined_bitfield = (static_cast<Uint16>(first_byte) << 8) | second_byte;
+
+			TileInstance new_tile_instance;
+			new_tile_instance.is_high_priority = (0x80 & first_byte) != 0;
+			new_tile_instance.palette_line = ((0x40 | 0x20) >> 5) & first_byte;
+			new_tile_instance.is_flipped_vertically = (0x10 & first_byte) != 0;
+			new_tile_instance.is_flipped_horizontally = (0x08 & first_byte) != 0;
+			new_tile_instance.tile_index = (static_cast<Uint16>((0x01 | 0x02 | 0x04) & first_byte) << 8) | second_byte;
+
+			new_layout->tile_instances.emplace_back(new_tile_instance);
+		}
+
+		return new_layout;
 	}
 }
