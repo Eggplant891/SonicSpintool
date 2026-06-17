f2c242a5729b4132ac8390e4c267dfb4110f4ba9
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2026-06-17 16:56:23 +0200

    Refactor Serialiser and Deserialiser class methods

--- redist/serialisation/editor_serialiser.h
+++ redist/serialisation/editor_serialiser.h
@@ -2,7 +2,10 @@
 
 #include "nlohmann/json.hpp"
 
+#include <filesystem>
 #include <fstream>
+#include <memory>
+#include <string_view>
 
 namespace spintool
 {
@@ -11,11 +14,23 @@ namespace spintool
     public:
         ~Serialiser();
 
-        [[nodiscard]] static std::unique_ptr<Serialiser> OpenFile(const std::filesystem::path& path);
-        [[nodiscard]] static std::unique_ptr<Serialiser> OpenFile(const std::filesystem::path& path, std::string_view filename);
+        [[nodiscard]] static std::unique_ptr<Serialiser> OpenFile(
+            const std::filesystem::path& path
+        );
 
-        [[nodiscard]] static bool FileExists(const std::filesystem::path &path);
-        [[nodiscard]] static bool FileExists(const std::filesystem::path& path, std::string_view filename);
+        [[nodiscard]] static std::unique_ptr<Serialiser> OpenFile(
+            const std::filesystem::path& path,
+            std::string_view filename
+        );
+
+        [[nodiscard]] static bool FileExists(
+            const std::filesystem::path& path
+        );
+
+        [[nodiscard]] static bool FileExists(
+            const std::filesystem::path& path,
+            std::string_view filename
+        );
 
         [[nodiscard]] nlohmann::json& Writer();
 
@@ -23,22 +38,33 @@ namespace spintool
 
     private:
         std::ofstream m_out_stream;
-        nlohmann::json m_json_writer;
+        nlohmann::json m_json_writer = nlohmann::json::object();
     };
 
     class Deserialiser
     {
     public:
-        [[nodiscard]] static std::unique_ptr<Deserialiser> OpenFile(const std::filesystem::path& path);
-        [[nodiscard]] static std::unique_ptr<Deserialiser> OpenFile(const std::filesystem::path &path, std::string_view filename);
+        [[nodiscard]] static std::unique_ptr<Deserialiser> OpenFile(
+            const std::filesystem::path& path
+        );
 
-        [[nodiscard]] static bool FileExists(const std::filesystem::path &path);
-        [[nodiscard]] static bool FileExists(const std::filesystem::path& path, std::string_view filename);
+        [[nodiscard]] static std::unique_ptr<Deserialiser> OpenFile(
+            const std::filesystem::path& path,
+            std::string_view filename
+        );
 
-        [[nodiscard]] const nlohmann::json& Reader();
+        [[nodiscard]] static bool FileExists(
+            const std::filesystem::path& path
+        );
+
+        [[nodiscard]] static bool FileExists(
+            const std::filesystem::path& path,
+            std::string_view filename
+        );
+
+        [[nodiscard]] const nlohmann::json& Reader() const;
 
     private:
-        std::ifstream m_in_stream;
-        nlohmann::json m_json_reader;
+        nlohmann::json m_json_reader = nlohmann::json::object();
     };
 }
